2da126db6aef1034a55d206df58a23659ef57d06
Vaishnavi-20110118 <125288249+Vaishnavi-20110118@users.noreply.github.com>
2023-07-25 15:58:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("b2").addEventListener('click', bf2);
+function bf2(){
+document.getElementById("car").src="https://imgd.aeplcdn.com/664x374/n/cw/ec/20865/amg-gt-exterior-right-front-three-quarter-60800.jpeg?q=75"
+}
+document.getElementById("b3").addEventListener('click', bf3);
+function bf3(){
+document.getElementById("car").src="https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+}
+document.getElementById("b1").addEventListener('click', bf1);
+function bf1(){
+document.getElementById("car").src="https://cdn.drivek.com/configurator-imgs/cars/it/$original$/MERCEDES/AMG-GT/40939_COUPE-5-DOORS/mercedes-amg-gt-63-s-e-performance-side-front.jpg"
+}
+document.getElementById("cart").addEventListener('click', car);
+function car(){
+document.getElementById("cart").innerHTML="Your item is added to cart";
+}
